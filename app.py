671fd8f2b441df60d297fca1322604cad3e45baf
SincyAbraham <137414913+SincyAbraham@users.noreply.github.com>
2023-07-05 14:49:24 +0100

    Update app.py
    Displaying messages from database

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, Response
+from datetime import datetime
 import pyodbc
+import json
 
 
 app = Flask(__name__)
@@ -20,7 +22,32 @@ cnxn = pyodbc.connect(
 def home():
     return 'Hello, World!'
 
+@app.route('/messages')
+def message():
+    # Create a cursor object to execute SQL queries
+    cursor = cnxn.cursor()
 
+    # Execute the SELECT statement
+    query = "SELECT * FROM messages"
+    cursor.execute(query)
+
+    # Fetch all rows from the result set
+    rows = cursor.fetchall()
+
+    column_names = [column[0] for column in cursor.description]
+    row_dicts = []
+    for row in rows:
+        print(row)
+        row_dict = dict(zip(column_names, row))
+        # Convert datetime objects to strings
+        for key, value in row_dict.items():
+            if isinstance(value, datetime):
+                row_dict[key] = value.strftime('%Y-%m-%dT%H:%M:%S')
+        row_dicts.append(row_dict)
+
+    # Close the cursor and database connection
+    cursor.close()    
+    return Response(json.dumps(row_dicts), mimetype='application/json')
 @app.route('/users')
 def user():
     # Create a cursor object to execute SQL queries
@@ -43,4 +70,3 @@ def user():
         return(row.userName+row.firstName)
 if __name__ == '__main__':
     app.run()
-
