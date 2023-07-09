b0849d199da7b20bfde20d58da5acfe6706d653f
jerinjoseputhussery <60233550+jerinjoseputhussery@users.noreply.github.com>
2023-07-09 15:49:08 +0100

    Update Database.sql
    Database script changes

--- Database.sql
+++ Database.sql
@@ -1,6 +1,7 @@
 create table users(userId int primary key,userName varchar(100) unique,firstName varchar(100),lastName varchar(100),emailId varchar(100) unique,password varchar(100),creationDateTime DATETIME); 
 --The table messages used to store messages
 create table messages(messageId int primary key, sentBy int foreign key references users(userId),recipientId int foreign key references users(userId),messageContent varchar(3000),messageCreationTime DATETIME,readStatus varchar(15) );
+create table friends (userId int foreign key references users(userId) ,friendId int foreign key references users(userId) primary key(userId,friendId));
 create SEQUENCE userSequence; 
 alter SEQUENCE userSequence RESTART WITH 1;
 
