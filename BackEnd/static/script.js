e04042b1f726ad876ad131b26f1f3670eeadd599
jerinjoseputhussery <60233550+jerinjoseputhussery@users.noreply.github.com>
2023-07-17 00:55:25 +0100

    Javascript
    Javascript added

---
+++ BackEnd/static/script.js
@@ -0,0 +1,88 @@
+document.getElementById('registrationForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    const firstname = document.getElementById('fname').value;
+    const lastname = document.getElementById('lname').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    // Create data object to be sent
+    const data = {
+        "firstName": firstname,
+        "lastName":lastname,
+        "emailId":email,
+        "password": password
+    };
+  
+    // POST request using fetch
+    fetch('/registerUser', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+      // Handle response data
+      console.log(data);
+    })
+    .catch(error => {
+      // Handle any errors
+      console.error('Error:', error);
+    });
+  });
+
+
+
+  document.getElementById('loginform').addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    
+    const email = document.getElementById('usernameForLogin').value;
+    const password = document.getElementById('passwordForLogin').value;
+    // Create data object to be sent
+    const data = {
+        
+        "userName":email,
+        "password": password
+    };
+  
+    // POST request using fetch
+    fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+      // Handle response data
+      console.log(data);
+      if(data.status==0){
+        console.log('LOGIN SUCCESS');
+      }else{
+        console.log('LOGIN FAILED');
+      }
+    })
+    .catch(error => {
+      // Handle any errors
+      console.error('Error:', error);
+    });
+  });
+
+  function showForm() 
+        {
+          var signInForm = document.getElementById("loginform");
+          var registerForm = document.getElementById("registrationForm");
+          signInForm.classList.add("hidden");
+          registerForm.classList.remove("hidden");
+        }
+        function showLoginForm() 
+        {
+          var signInForm = document.getElementById("loginform");
+          var registerForm = document.getElementById("registrationForm");
+          signInForm.classList.remove("hidden");
+          registerForm.classList.add("hidden");
+        }
+  
