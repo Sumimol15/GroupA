d2806db60d6d249034d2d49f7121dab04b8261cf
SincyAbraham <137414913+SincyAbraham@users.noreply.github.com>
2023-07-17 15:44:02 +0100

    Recent contacts displayed
    Recent contacts displayed

--- BackEnd/static/messageScript.js
+++ BackEnd/static/messageScript.js
@@ -1,20 +1,30 @@
 window.onload=function() {
+    const contactsSection = document.getElementById('contacts');
     var queryString = window.location.search;
     var urlParams = new URLSearchParams(queryString);
     var userId = urlParams.get('userId');
     console.log(userId);
-    // const data= GIve data json here
+    const jsonData= {"userId":userId}
     fetch('/getMyFriends', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify(jsonData)
       })
       .then(response => response.json())
       .then(data => {
         // Handle response data
         console.log(data);
+        
+        // contactsSection.appendChild(`<ul>`)
+        data.forEach(element => {
+            const contactDiv = document.createElement('div');
+            contactDiv.className = 'contactsDiv';
+            contactDiv.innerHTML = `${element.userName}`
+            contactsSection.appendChild(contactDiv);
+        });
+        // contactsSection.appendChild(`</ul>`)
       })
       .catch(error => {
         // Handle any errors
